cd05283bd7b2ab0b1a03226552949cb99ec6d74a
luangelsdorf <luanferreira2136@gmail.com>
2021-09-26 10:54:51 -0300

    Added category links

--- src/components/blog/PostHero/index.js
+++ src/components/blog/PostHero/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import styles from './PostHero.module.scss';
+import Link from 'next/link';
 
 export default function PostHero({ postContent }) {
 
   function formatCategories() {
     let categories = [];
     postContent.categories.map(category => {
-      categories.push(category.name);
+      categories.push(``);
     });
     return categories.join(', ')
   }
 
   return (
-    <section className={styles.section} style={{backgroundImage: `url(${process.env.NEXT_PUBLIC_API_URL}${postContent.cover.url})`}}>
+    <section className={styles.section} style={{ backgroundImage: `url(${process.env.NEXT_PUBLIC_API_URL}${postContent.cover.url})` }}>
       <div className="container">
         <div className={styles.heroText}>
           <h1>{postContent.title}</h1>
@@ -20,7 +21,16 @@ export default function PostHero({ postContent }) {
             <img height="17" width="17" src="/images/new/tags.svg" alt="Tags" />
             <span className="caption-upper">
               {
-                formatCategories()
+                postContent.categories.map((cat, index) => {
+                  return (
+                    <Link key={`cat-${index}`} href={`/categorias/${cat.slug}`}>
+                      <a>
+                        {cat.name}
+                        { postContent.categories.length -1 === index ? null : ', ' }
+                      </a>
+                    </Link>
+                  )
+                })
               }
             </span>
           </div>
