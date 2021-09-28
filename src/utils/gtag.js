961e52681baf481bee3a9503989d7ad694f0d1ee
luangelsdorf <luanferreira2136@gmail.com>
2021-09-28 16:13:19 -0300

    Added GA4 ID

--- src/utils/gtag.js
+++ src/utils/gtag.js
@@ -1,5 +1,8 @@
 export const pageView = (url) => {
   window.gtag('config', 'UA-181473567-1', {
     page_path: url,
-  })
+  });
+  window.gtag('config', 'G-R0H77YJN3F', {
+    page_path: url,
+  });
 }
