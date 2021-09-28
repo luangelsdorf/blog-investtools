e109b91d25f10c1d20f65c4e5ce9cd8602603942
luangelsdorf <luanferreira2136@gmail.com>
2021-09-28 15:04:57 -0300

    Added meta tags and GA scripts

---
+++ src/components/common/PageScripts.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function PageScripts() {
+  return (
+    <>
+      {/*Global site tag (gtag.js) - Google Analytics*/}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-181473567-1"/>
+      <script dangerouslySetInnerHTML={{
+        __html:
+          `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-181473567-1');`
+      }}/>
+
+      {/*Google Tag Manager*/}
+      <script dangerouslySetInnerHTML={{
+        __html:
+        `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PXJ6DDW');`
+      }} />
+    </>
+  )
+}
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,32 +1,33 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import PageScripts from 'src/components/common/PageScripts'
 
 class MyDocument extends Document {
   ogTags = {
-    title: 'Investtools',
-    site_name: 'Investtools',
+    title: 'Blog Investtools',
+    site_name: 'Blog Investtools',
     url: 'blog.investtools.com.br',
     description: 'A Investtools é uma empresa com foco nas melhores soluções de tecnologia para o mercado financeiro.',
     type: 'website',
-    image: '/images/share/comp-Investtools.jpg',
+    image: '/images/comp-Investtools.jpg',
   }
 
   render() {
     return (
       <Html>
         <Head>
-          {/* <PageScripts /> */}
+          <PageScripts />
           {/*Metadata*/}
           <meta charSet="utf-8" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black" />
           <meta name="HandHeldFriendly" content="True" />
           {/*OG Tags*/}
-          {/* <meta property="og:title"       content={this.ogTags.title} />
+          <meta property="og:title"       content={this.ogTags.title} />
           <meta property="og:site_name"   content={this.ogTags.site_name} />
           <meta property="og:url"         content={this.ogTags.url} />
           <meta property="og:description" content={this.ogTags.description} />
           <meta property="og:type"        content={this.ogTags.type} />
-          <meta property="og:image"       content={this.ogTags.image} /> */}
+          <meta property="og:image"       content={this.ogTags.image} />
           {/*Favicon*/}
           <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" sizes="180x180" />
           <link rel="icon" href="/favicon/favicon-32x32.png" sizes="32x32" type="image/png" />
